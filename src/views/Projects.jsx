9394d293acd9a22ba5a94b663ac40330552af536
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-05-07 14:47:34 +0530

    push

---
+++ src/views/Projects.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import Card from "../components/Card";
+import Card1 from "../components/Card1";
+import Card2 from "../components/Card2";
+
+import { ThemeContext } from "../themeProvider";
+
+const Projects = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div
+      id="projects"
+      className={darkMode ? "bg-white text-black" : "bg-gray-900 text-white"}
+    >
+      <div className="max-w-7xl mx-auto x-4 sm:px-6 lg:px-8 px-4  pt-24 pb-12">
+        <h2 className="text-5xl font-bold px-4 md:px-0 text-center">
+          Projects
+        </h2>
+        <h4 className="mt-16 text-3xl font-semibold text-blue-600">
+          What I Built
+        </h4>
+        <div className="mt-8 flex justify-between items-stretch flex-wrap">
+          <Card />
+          <Card1 />
+          <Card2 />
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
