5f33212b99eea196c47e622ab01d8bf8d42d3c1e
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2022-12-13 11:09:13 +0530

    link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -41,7 +41,7 @@ const Card = () => {
         <div
           style={{ marginRight: "40px" }}
           onClick={() => {
-            const url = "https://www.myntra.com/mens-shorts"
+            const url = "https://fabulous-queijadas-f98643.netlify.app/"
             const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
             if (newWindow) newWindow.opener = null
 
